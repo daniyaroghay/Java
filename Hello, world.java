0c95ce54fb3550bd66bb2a76db50eebfc49a3927
Daniya Roghay <w.daniyaroghay@gmail.com>
2025-05-01 18:42:03 +0530

    Add files via upload

---
+++ Hello, world.java
@@ -0,0 +1,20 @@
+public class Main {
+    
+    public static void main (String[] args) {
+        
+        //This is a comment
+        
+        /*
+            This
+            is
+            a
+            multi-line
+            comment
+            */
+        
+        System.out.println("Hello, World!");
+        System.out.println("Next line.");
+        System.out.println("Third line.");
+        
+    }
+}
---
+++ variables.java
@@ -0,0 +1,30 @@
+public class Main {
+    
+    public static void main (String[] args) {
+        
+        int year = 2025;
+        int age = 19;
+        
+        double price = 4.99;
+        double cgpa = 8.2;
+        
+        char grade = 'A';
+        char operator = '*';
+        
+        boolean isStudent = true;
+        boolean forSale = false;
+        
+        String name = "Umm Ibraheem";
+        String color = "Black";
+        String car = "Mustang";
+        
+        System.out.println("Your name is: " + name);
+        System.out.println("Your age is: " + age);
+        System.out.println("You are a student: " + isStudent);
+        System.out.println("Your cgpa is: " + cgpa);
+        System.out.println("Your grade is: " + grade);
+        
+        System.out.println("Your choice of car is a " + color + " " + year + " " + car);
+        
+    }
+}
